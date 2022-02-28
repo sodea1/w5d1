7bfdfd4023d88ee40f274646f0c9f4d6f6df0dc5
seanodea <seanodea11@gmail.com>
2022-02-28 17:13:02 -0500

    linked list complete

--- LRU_cache_project_skeleton/lib/p04_linked_list.rb
+++ LRU_cache_project_skeleton/lib/p04_linked_list.rb
@@ -1,3 +1,4 @@
+require "byebug"
 class Node
   attr_reader :key
   attr_accessor :val, :next, :prev
@@ -14,13 +15,24 @@ class Node
   end
 
   def remove
+    prev_node = self.prev
+    next_node = self.next
+    prev_node.next = next_node
+    next_node.prev = prev_node
     # optional but useful, connects previous link to next link
     # and removes self from list.
   end
 end
 
 class LinkedList
+
+  include Enumerable
+
   def initialize
+    @head = Node.new
+    @tail = Node.new
+    @head.next = @tail
+    @tail.prev = @head
   end
 
   def [](i)
@@ -29,30 +41,72 @@ class LinkedList
   end
 
   def first
+    @head.next
   end
 
   def last
+    @tail.prev
   end
 
   def empty?
+    return true if @head.next == @tail
+    false
   end
 
   def get(key)
+    self.each do |node| # how does this each function work
+      return node.val if node.key == key
+    end
+    nil
   end
 
   def include?(key)
+    self.each do |node|
+      return true if node.key == key
+    end
+
+    false
   end
 
   def append(key, val)
+    node = Node.new(key, val)
+
+    if @head.next == @tail
+      @head.next = node
+      node.prev = @head
+      node.next = @tail
+      @tail.prev = node
+    else
+      sec_last_node = self.last  # fetches @tail.prev node
+      sec_last_node.next = node
+      node.prev = sec_last_node
+      node.next = @tail
+      @tail.prev = node
+    end
+    
   end
 
   def update(key, val)
+    self.each { |node| node.val = val if node.key == key }
   end
 
   def remove(key)
+    self.each do |node|
+      if node.key == key
+        node.remove
+      end
+    end
   end
 
   def each
+    # debugger
+    node = @head.next
+
+    until node == @tail
+      yield node
+      node = node.next
+    end
+
   end
 
   # uncomment when you have `each` working and `Enumerable` included
