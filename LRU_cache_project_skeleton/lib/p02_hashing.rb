6b688fc130d1a7f1bb4e7312ac0778810db05030
seanodea <seanodea11@gmail.com>
2022-02-28 14:35:52 -0500

    hashing complete

--- LRU_cache_project_skeleton/lib/p02_hashing.rb
+++ LRU_cache_project_skeleton/lib/p02_hashing.rb
@@ -4,11 +4,29 @@ end
 
 class Array
   def hash
+    key = nil
+
+    # self.length.times do
+    #   ele = self.sample
+
+    self.each_with_index do |el, i|
+      key = el.hash ^ i.hash
+    end
+    # key.to_i(2).hash
+    key.hash
   end
 end
 
 class String
   def hash
+    key = nil
+    alpha = ("a".."z").to_a
+
+    self.chars.each_with_index do |char, i|
+      key = alpha.index(char) ^ i.hash
+    end
+
+    key.hash
   end
 end
 
@@ -16,6 +34,14 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    key = nil
+    array = self.to_a
+    array.sort_by(&:hash).hash
+
+    # array.flatten.each.with_index do |ele, i|
+    #   key = ele ^ i.hash
+    # end
+    # key
+    
   end
 end
